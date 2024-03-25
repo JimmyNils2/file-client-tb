240f751cd6d831816b6ad4f3b02de9d1468d46fa
Darelis Quintero <jimmynils2@gmail.com>
2024-03-25 08:53:43 -0300

    Search Bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import { Container, Navbar } from 'react-bootstrap';
+import { Container, Navbar, Form, Col, Row, Button } from 'react-bootstrap';
 
 export const Header = ({appName}) => {
   return (
     <Navbar data-bs-theme='dark' style={{ backgroundColor: `#ff6666` }}>
       <Container>
-        <Navbar.Brand href="#">{appName}</Navbar.Brand>
+        <Navbar.Brand href='#'>{appName}</Navbar.Brand>
+        <Form inline>
+        <Row>
+          <Col xs='auto'>
+            <Form.Control
+              type='text'
+              placeholder='Search a file'
+              className=' mr-sm-2'
+              variant='light'
+            />
+          </Col>
+          <Col xs='auto'>
+            <Button type='submit' variant='dark'>Search</Button>
+          </Col>
+        </Row>
+      </Form>
       </Container>
     </Navbar>
   )
